3c26d9d5fd7b79bedfe5e8f3af6dd1efa8d09b08
Ian Y.E. Pan <ianpan870102@gmail.com>
2021-04-20 19:51:28 +0800

    Remove font-family customization for Neotree

--- tron-legacy-theme.el
+++ tron-legacy-theme.el
@@ -517,11 +517,11 @@
    `(diff-hl-change                           ((t (:background ,vc-b :foreground ,vc-b))))
 
    ;;;;; Neo tree
-   `(neo-dir-link-face                        ((t (:foreground "#cccccc" :family "Sans Serif"))))
-   `(neo-header-face                          ((t (:foreground "#cccccc" :family "Sans Serif"))))
-   `(neo-banner-face                          ((t (:foreground "#cccccc" :family "Sans Serif"))))
-   `(neo-root-dir-face                        ((t (:foreground "#cccccc" :family "Sans Serif"))))
-   `(neo-file-link-face                       ((t (:foreground "#aaaaaa" :family "Sans Serif"))))
+   `(neo-dir-link-face                        ((t (:foreground "#cccccc"))))
+   `(neo-header-face                          ((t (:foreground "#cccccc"))))
+   `(neo-banner-face                          ((t (:foreground "#cccccc"))))
+   `(neo-root-dir-face                        ((t (:foreground "#cccccc"))))
+   `(neo-file-link-face                       ((t (:foreground "#aaaaaa"))))
    `(neo-expand-btn-face                      ((t (:foreground "#aaaaaa"))))
 
    ;;;;; smart mode line
