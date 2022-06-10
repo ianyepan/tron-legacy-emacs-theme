72655ce5ea0580cc23966821d2813e4d5e961f93
Ian Y.E. Pan <ianpan870102@gmail.com>
2022-06-10 10:46:28 -0700

    Add subtle bg color to org-block

--- tron-legacy-theme.el
+++ tron-legacy-theme.el
@@ -173,7 +173,7 @@
    `(org-footnote                             ((,class (:underline t :foreground ,fg4))))
    `(org-link                                 ((,class (:underline t :foreground ,type ))))
    `(org-special-keyword                      ((,class (:foreground ,tron-green))))
-   `(org-block                                ((,class (:foreground ,fg2 :background ,bg0 :extend t))))
+   `(org-block                                ((,class (:foreground ,fg2 :background ,bg2 :extend t))))
    `(org-quote                                ((,class (:inherit org-block :slant italic))))
    `(org-verse                                ((,class (:inherit org-block :slant italic))))
    `(org-todo                                 ((,class (:box (:line-width 1 :color ,tron-lightred)
